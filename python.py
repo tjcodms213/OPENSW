4c9ce28ad6ce00f03b73c7c63b4cf17b87ee9e38
tjcodms213 <tjcodms0213@naver.com>
2025-11-07 10:26:58 +0900

    Create python.py

---
+++ python.py
@@ -0,0 +1,14 @@
+import cv2
+
+src = cv2.imread("C:\image\img.jpg", cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+sobel = cv2.Sobel(gray, cv2.CV_8U, 1, 0, 3)
+laplacian = cv2.Laplacian(gray, cv2.CV_8U, ksize=3)
+canny = cv2.Canny(src, 100, 255)
+
+cv2.imshow("sobel", sobel)
+cv2.imshow("laplacian", laplacian)
+cv2.imshow("canny", canny)
+cv2.waitKey()
+cv2.destroyAllWindows()
